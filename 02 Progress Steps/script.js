20aec9a961402f2ca7b9164d5864f58119e62239
Carlos Rene Ponce Novelo <crpn81@yahoo.com.mx>
2026-08-24 15:07:51 -0600

    Rotating navigation finished.

--- 02 Progress Steps/script.js
+++ 02 Progress Steps/script.js
@@ -5,6 +5,7 @@ const circles = document.querySelectorAll('.circle')
 
 let currentActive = 1;
 let steps = circles.length;
+let stepPctRate = 100 / (steps - 1);
 
 btnNext.addEventListener('click', () => {
     currentActive++;
@@ -13,14 +14,13 @@ btnNext.addEventListener('click', () => {
         btnNext.disabled = true;
     }
     circles[currentActive - 1].classList.add('active');
-    progress.style.width = `${(currentActive - 1) * 33.333333}%`;
-    console.log(progress.width);
+    progress.style.width = `${(currentActive - 1) * stepPctRate}%`;
     btnPrev.disabled = false;
 });
 
 btnPrev.addEventListener('click', () => {
     circles[currentActive - 1].classList.remove('active');
-    progress.style.width = `${100 - (steps - currentActive + 1) * 33.333333}%`;
+    progress.style.width = `${100 - (steps - currentActive + 1) * stepPctRate}%`;
     currentActive--;
     if (currentActive <= 1) {
         currentActive = 1;
